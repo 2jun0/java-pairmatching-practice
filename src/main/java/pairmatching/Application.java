299c15ff33dbf2154664d9c24c0bf5e97b75bde8
2jun0 <soo28819@gmail.com>
2022-12-11 18:40:37 +0900

    Feat: 컨트롤러 실행 및 종속성 주입.

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,22 @@
 package pairmatching;
 
+import pairmatching.controller.PairMatchingController;
+import pairmatching.view.InputFormatter;
+import pairmatching.view.InputParser;
+import pairmatching.view.InputValidator;
+import pairmatching.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        PairMatchingController controller = new PairMatchingController(inputView());
+        controller.start();
+    }
+
+    private static InputView inputView() {
+        InputValidator inputValidator = new InputValidator();
+        InputParser inputParser = new InputParser(inputValidator);
+        InputFormatter inputFormatter = new InputFormatter();
+
+        return new InputView(inputValidator, inputFormatter, inputParser);
     }
 }
