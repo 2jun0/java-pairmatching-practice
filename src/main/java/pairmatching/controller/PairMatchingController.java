6a7a8e09c0216ba5cd3cb9b9c4141f09b92b3694
2jun0 <soo28819@gmail.com>
2022-12-11 16:11:39 +0900

    Feat(기능 선택): 페어 매칭 기능 실행 구현.

---
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -0,0 +1,24 @@
+package pairmatching.controller;
+
+import pairmatching.view.InputView;
+
+public class PairMatchingController {
+
+    private final InputView inputView;
+
+    public PairMatchingController(InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public void start() {
+        String command = inputView.inputFunctionSelectCommand();
+
+        if ("1".equals(command)) {
+            startPairMatch();
+        }
+    }
+
+    private void startPairMatch() {
+        String command = inputView.inputMissionSelectCommand();
+    }
+}
