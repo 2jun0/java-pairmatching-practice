4b21305aca5a181fe72e8def971727f192f6bf9e
2jun0 <soo28819@gmail.com>
2022-12-12 23:12:10 +0900

    Refactor(CrewPair): 페어매칭을 서비스에게 하도록 함.

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -1,14 +1,26 @@
 package pairmatching.controller;
 
-import pairmatching.dto.MissionSelectCommandDTO;
+import java.util.List;
+import pairmatching.domain.Course;
+import pairmatching.domain.CrewPair;
+import pairmatching.domain.Level;
+import pairmatching.domain.Mission;
+import pairmatching.dto.MissionSelectCommandDto;
+import pairmatching.service.CrewPairService;
+import pairmatching.service.CrewService;
 import pairmatching.view.InputView;
 
 public class PairMatchingController {
 
     private final InputView inputView;
+    private final CrewService crewService;
+    private final CrewPairService crewPairService;
 
-    public PairMatchingController(InputView inputView) {
+    public PairMatchingController(InputView inputView, CrewService crewService,
+            CrewPairService crewPairService) {
         this.inputView = inputView;
+        this.crewService = crewService;
+        this.crewPairService = crewPairService;
     }
 
     public void start() {
@@ -20,6 +32,12 @@ public class PairMatchingController {
     }
 
     private void startPairMatch() {
-        MissionSelectCommandDTO commands = inputView.inputMissionSelectCommands();
+        MissionSelectCommandDto commandDto = inputView.inputMissionSelectCommands();
+        Level level = commandDto.level();
+        Course course = commandDto.course();
+        Mission mission = commandDto.mission();
+
+        List<CrewPair> crewPairs = crewPairService.pairMatch(level, course);
+        crewPairService.saveCrewPairs(level, crewPairs);
     }
 }
--- src/main/java/pairmatching/service/CrewPairService.java
+++ src/main/java/pairmatching/service/CrewPairService.java
@@ -1,8 +1,11 @@
 package pairmatching.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import pairmatching.domain.Course;
 import pairmatching.domain.Crew;
 import pairmatching.domain.CrewPair;
 import pairmatching.domain.Level;
@@ -11,9 +14,39 @@ import pairmatching.repository.CrewPairRepository;
 public class CrewPairService {
 
     private final CrewPairRepository crewPairRepository;
+    private final CrewService crewService;
 
-    public CrewPairService(CrewPairRepository crewPairRepository) {
+    public CrewPairService(CrewPairRepository crewPairRepository, CrewService crewService) {
         this.crewPairRepository = crewPairRepository;
+        this.crewService = crewService;
+    }
+
+    public List<CrewPair> pairMatch(Level level, Course course) {
+        for (int i = 0; i < 3; i++) {
+            List<CrewPair> crewPairs = tryPairMatch(course);
+
+            if (!isAlreadyPairMatchedMany(level, crewPairs)) {
+                return crewPairs;
+            }
+        }
+
+        throw new IllegalArgumentException("폐어 매칭 실패");
+    }
+
+    private List<CrewPair> tryPairMatch(Course course) {
+        List<Crew> crews = crewService.getCrews(course);
+        List<CrewPair> crewPairs = new ArrayList<>();
+
+        for (int i = 0; i < crews.size(); i+=2) {
+            if (i == crews.size() - 3) {
+                crewPairs.add(new CrewPair(crews.get(i), crews.get(i+1), crews.get(i+2)));
+                break;
+            }
+
+            crewPairs.add(new CrewPair(crews.get(i), crews.get(i+1)));
+        }
+
+        return crewPairs;
     }
 
     public void saveCrewPairs(Level level, List<CrewPair> crewPairs) {
@@ -22,7 +55,12 @@ public class CrewPairService {
         }
     }
 
-    public boolean isAlreadyPairMatched(Level level, CrewPair crewPair) {
+    private boolean isAlreadyPairMatchedMany(Level level, List<CrewPair> crewPairs) {
+        return crewPairs.stream()
+                .anyMatch(crewPair -> isAlreadyPairMatchedOne(level, crewPair));
+    }
+
+    private boolean isAlreadyPairMatchedOne(Level level, CrewPair crewPair) {
         List<CrewPair> crewPairs = crewPairRepository.findByLevel(level);
 
         return crewPairs.stream()
