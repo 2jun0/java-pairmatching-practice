cd6635a2885db7ccf7d200b831f58e2ab515484a
2jun0 <soo28819@gmail.com>
2022-12-12 23:07:42 +0900

    Refactor(Util): URL대신 소스패스로 변경.

--- src/main/java/pairmatching/utils/ResourceReader.java
+++ src/main/java/pairmatching/utils/ResourceReader.java
@@ -1,20 +1,15 @@
 package pairmatching.utils;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ResourceReader {
 
     public static List<String> readResource(String name) throws IOException {
-        URL resource = ResourceReader.class.getClassLoader().getResource(name);
-        assert resource != null;
-
-        BufferedReader bufferedReader = new BufferedReader(
-                new InputStreamReader(resource.openStream()));
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/" + name));
 
         return bufferedReader.lines().collect(Collectors.toList());
     }
