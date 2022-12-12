ab6b9eb7073c3b7b1e981c5c6de1f294a1f1bc8a
2jun0 <soo28819@gmail.com>
2022-12-12 23:54:45 +0900

    Fix(입력 검증): Q가 탐지되지 않는 오류 수정.

--- src/main/java/pairmatching/view/InputValidator.java
+++ src/main/java/pairmatching/view/InputValidator.java
@@ -7,7 +7,7 @@ import pairmatching.domain.Mission;
 public class InputValidator {
 
     void validateFunctionSelectCommand(String command) {
-        if (!command.matches("^1|2|3|q$")) {
+        if (!command.matches("^1|2|3|Q$")) {
             throw new IllegalArgumentException("잘못 입력하셨습니다. 다시 입력해주세요.");
         }
     }
